96edc2046e44baca47d10b3ddaeba4be89f45ac3
Emily Cheng <echeng06@uw.edu>
2025-01-15 16:42:40 -0800

    Writing legacy sanity checker

--- gear/legacy_sanity_check/src/python/legacy_sanity_check_app/main.py
+++ gear/legacy_sanity_check/src/python/legacy_sanity_check_app/main.py
@@ -2,16 +2,94 @@
 
 import logging
 
+from typing import List
+
+import flywheel
+from datastore.forms_store import FormsStoreGeneric
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from key.keys import (
+    DefaultValues,
+    FieldNames,
+    SysErrorCodes,
+    preprocess_errors,
+)
+from outputs.errors import ListErrorWriter, preprocessing_error
+from preprocess.preprocessor import FormProjectConfigs
 
 log = logging.getLogger(__name__)
 
+
+class LegacySanityChecker:
+    """Class to run sanity checks on legacy retrospective projects."""
+
+    def __init__(self,
+                 form_store: FormStoreGeneric,
+                 form_configs: FormProjectConfigs,
+                 error_writer: ListErrorWriter):
+        """Initializer."""
+        self.__form_store = form_store
+        self.__form_configs = form_configs
+        self.__error_writer = error_writer
+
+    def check_multiple_ivp(self,
+                           subject_lbl: str,
+                           module: str) -> None:
+        """Checks that a subject does not have multiple
+        initial visits
+
+        Args:
+            subject_lbl: The subject (NACCID)
+            module: The module (e.g. UDS)
+        """
+        log.info(f'Checking for multiple visits for {subject_lbl} '
+                 + f' module {module}')
+
+        module_configs = self.__form_configs.module_configs.get(module, None)
+        if not module_configs:
+            raise ValueError(f"Unrecognized module: {module}")
+
+        initial_packets = self.__form_store.query_project(
+            subject_lbl=subject_lbl,
+            module=module,
+            search_col=FieldNames.PACKET,
+            search_val=module_configs.initial_packets,
+            search_op=DefaultValues.FW_SEARCH_OR)  # type: ignore
+
+        num_initial_packets = len(initial_packets) if initial_packets else 0
+        if num_initial_packets > 1:
+            log.error(f"Subject {subject_lbl} has multiple initial visit packets "
+                      + f"for {module} {num_initial_packets}, writing errors")
+            self.__error_writer.write(
+                FileError(
+                    error_type='error',
+                    error_code=SysErrorCodes.MULTIPLE_IVP,
+                    value=f'{num_initial_packets} visits',
+                    message=preprocess_errors[SysErrorCodes.MULTIPLE_IVP]
+                )
+            )
+
+    def run_all_checks(self, subject_lbl: str) -> None:
+        """Runs all sanity checks on the given subject.
+
+        Args:
+            subject_lbl: The subject's label
+        """
+        module_configs = self.__form_configs.module_configs
+        for module in module_configs:
+            self.check_multiple_ivp(subject_lbl, module)
+
+
 def run(*,
         proxy: FlywheelProxy,
-        new_only: bool = False):
-    """Runs the Legacy Sanity Check process.
+        sanity_checker: LegacySanityChecker,
+        project: flywheel.Project):
+    """Runs the Legacy Sanity Check process for each subject
+    in the project.
 
     Args:
         proxy: the proxy for the Flywheel instance
+        sanity_checker: The LegacySanityChecker
+        project: The Flywheel project to evaluate
     """
-    pass
+    for subject in project.subjects():
+        sanity_checker.run_all_checks(subject.label)
--- gear/legacy_sanity_check/src/python/legacy_sanity_check_app/run.py
+++ gear/legacy_sanity_check/src/python/legacy_sanity_check_app/run.py
@@ -4,6 +4,7 @@ import logging
 
 from typing import Optional
 
+from datastore.forms_store import FormsStoreGeneric
 from flywheel_gear_toolkit import GearToolkitContext
 from gear_execution.gear_execution import (
     ClientWrapper,
@@ -11,16 +12,27 @@ from gear_execution.gear_execution import (
     GearEngine,
     GearExecutionEnvironment,
 )
-from legacy_sanity_check_app.main import run
+from legacy_sanity_check_app.main import (
+    LegacySanityChecker,
+    run,
+)
 from inputs.parameter_store import ParameterStore
+from outputs.errors import ListErrorWriter
+from preprocess.preprocessor import FormProjectConfigs
+from pydantic import ValidationError
 
 log = logging.getLogger(__name__)
 
 class LegacySanityCheckVisitor(GearExecutionEnvironment):
     """Visitor for the Legacy Sanity Check gear."""
 
-    def __init__(self, admin_id: str, client: ClientWrapper, new_only: bool):
+    def __init__(self,
+                 file_input: InputFileWrapper,
+                 form_configs_input: InputFileWrapper):
         super().__init__(client=client, admin_id=admin_id)
+
+        self.__file_input = file_input
+        self.__form_configs_input = form_configs_input
 
     @classmethod
     def create(
@@ -43,14 +55,55 @@ class LegacySanityCheckVisitor(GearExecutionEnvironment):
         client = GearBotClient.create(context=context,
                                       parameter_store=parameter_store)
 
+        file_input = InputFileWrapper.create(input_name='input_file',
+                                             context=context)
+        assert file_input, "missing expected input, input_file"
+
+        form_configs_input = InputFileWrapper.create(
+            input_name='form_configs_file', context=context)
+        assert form_configs_input, "missing expected input, form_configs_file"
+
         return LegacySanityCheckVisitor(
-            admin_id=context.config.get("admin_group", "nacc"),
-            client=client,
-            new_only=context.config.get("new_only", False))
+            file_input=file_input,
+            form_configs_input=form_configs_input)
 
     def run(self, context: GearToolkitContext) -> None:
+        """Run the Legacy Sanity Checker"""
+        file_id = self.__file_input.file_id
+        try:
+            file = proxy.get_file(file_id)
+        except ApiException as error:
+            raise GearExecutionError(
+                f'Failed to find the input file: {error}') from error
+
+        project = self.__file_input.get_parent_project(
+            self.proxy, file=file)
+
+        error_writer = ListErrorWriter(
+            container_id=file_id,
+            fw_path=self.proxy.get_lookup_path(file))
+
+        form_configs = None
+        with open(self.__form_configs_input.filepath, mode='r') as fh:
+            form_configs = None
+            try:
+                form_configs = FormProjectConfigs.model_validate_json(
+                    fh.read())
+            except ValidationError as error:
+                raise GearExecutionError(
+                    'Error reading form configurations file'
+                    f'{self.__form_configs_input.filename}: {error}') from error
+
+        sanity_checker = LegacySanityChecker(
+            form_store=FormsStoreGeneric(project=project),
+            form_configs=form_configs,
+            error_writer=error_writer)
+
         run(proxy=self.proxy,
-            new_only=self.__new_only)
+            sanity_checker=sanity_checker,
+            project=project)
+
+        # TODO: WRITE ERRORS TO LOG FILE
 
 def main():
     """Main method for Legacy Sanity Check."""
