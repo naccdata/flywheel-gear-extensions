4b0e949299dc400722f1d9ae9ea8550dfb43858b
Ben Keller <bjkeller@vt.edu>
2025-02-07 16:48:32 +0000

    Update symbol table

--- common/test/python/curator/test_symbol_table.py
+++ common/test/python/curator/test_symbol_table.py
@@ -13,3 +13,43 @@ class TestSymbolTable:
         assert table.get('a.b') == {'c': 1}
         assert table.get('a.b.c') == 1
         assert table.get('a.b.c.d') is None
+
+    def test_set(self):
+        table = SymbolTable()
+        table['a.b.c'] = {'d': 1}
+        assert table.get('a.b.c.d') == 1
+        assert table['a.b.c.d'] == 1
+
+        table['a.b.e.d'] = 2
+        assert table.get('a.b.e.d') == 2
+
+        table['a.b.c.d'] = 3
+        assert table.get('a.b.c.d') == 3
+
+    def test_initialized(self):
+        table = SymbolTable({
+            'a': {
+                'b': {
+                    'x': 1
+                }
+            },
+            's': {
+                'a': {
+                    'y': 2,
+                    'd': ['alpha', 'beta']
+                }
+            }
+        })
+        assert table.get('a.b.x') == 1
+        assert table.get('s.a.y') == 2
+        table['a.b.c'] = {'d': 1}
+        assert table.get('a.b.c.d') == 1
+        assert table['a.b.c.d'] == 1
+
+        table['a.b.e.d'] = 2
+        assert table.get('a.b.e.d') == 2
+
+        table['a.b.c.d'] = 3
+        assert table.get('a.b.c.d') == 3
+
+        assert table.get('s.a.d') == ['alpha', 'beta']
