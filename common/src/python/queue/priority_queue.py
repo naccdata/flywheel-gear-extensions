d14fdd5cca78bb0fac30ba7acdb310d20d35bf83
Ben Keller <bjkeller@vt.edu>
2025-02-13 23:49:15 +0000

    Add priority queue class

---
+++ common/src/python/queue/priority_queue.py
@@ -0,0 +1,23 @@
+from heapq import heappop, heappush
+from queue.comparable import Comparable
+from typing import Generic, List, Optional, TypeVar
+
+T = TypeVar('T', bound=Comparable)
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self.__minheap: List[T]= []
+
+    def __len__(self):
+        return len(self.__minheap)
+
+    def push(self, element: T) -> None:
+        heappush(self.__minheap, element)
+
+    def pop(self) -> Optional[T]:
+        if len(self.__minheap) > 0:
+            return heappop(self.__minheap)
+
+        return None
+
+    def empty(self) -> bool:
+        return not self.__minheap
