f01cea853f69c98304eaa6da16113dfe72f5b12f
Ben Keller <bjkeller@vt.edu>
2025-02-26 20:33:54 +0000

    Update gear uds-curator definition to match changes

--- gear/uds_curator/src/python/uds_curator_app/uds_curator.py
+++ gear/uds_curator/src/python/uds_curator_app/uds_curator.py
@@ -1,20 +1,20 @@
 import logging
 import re
 from datetime import datetime
-from typing import Any, Dict, List, Optional
+from typing import Optional
 
+from curator.form_curator import FormCurator
 from curator.symbol_table import SymbolTable
+from files.form import Form
 from files.uds_form import UDSV3Form, datetime_from_form_date
-from flywheel.models.file_entry import FileEntry
 from flywheel.models.subject import Subject
 from flywheel_gear_toolkit import GearToolkitContext
-from flywheel_gear_toolkit.utils.curator import FileCurator
 
 log = logging.getLogger(__name__)
 # log.setLevel("DEBUG")
 
 
-class UDSFileCurator(FileCurator):
+class UDSFileCurator(FormCurator):
     """birthyr/birthmo/visitdate, file.info.derived.naccage
     rac/racesec/raceter, file.info.derived.naccnihr.
 
@@ -36,39 +36,56 @@ class UDSFileCurator(FileCurator):
       - replace object if session date is newer
     """
 
-    def __init__(self,
-                 context: Optional[GearToolkitContext] = None,
-                 extra_packages: Optional[List[str]] = None,
-                 **kwargs) -> None:  # type: ignore
-        super().__init__(context, extra_packages, **kwargs)
+    def __init__(self, context: GearToolkitContext) -> None:  # type: ignore
+        super().__init__(context)
         self.__symbol_table = SymbolTable()
 
-    def get_subject(self, file_entry: FileEntry) -> Subject:
-        """Get the subject for the file entry.
+    # def get_subject(self, file_entry: FileEntry) -> Subject:
+    #     """Get the subject for the file entry.
 
-        Args:
-          file_entry: the file entry
-        Returns:
-          the Subject for the file entry
-        """
-        parents_subject = file_entry.parents.get("subject")
-        return self.context.client.get_subject(parents_subject)
+    #     Args:
+    #       file_entry: the file entry
+    #     Returns:
+    #       the Subject for the file entry
+    #     """
+    #     parents_subject = file_entry.parents.get("subject")
+    #     return self.context.client.get_subject(parents_subject)
 
-    def curate_form(self, form: UDSV3Form) -> None:
+    def curate_form(self, form: Form) -> None:
         """Computes and sets derived variables for the form."""
-        if not form.is_initial_visit():
+        if not form:
             return
 
-        form_date = form.get_form_date(
-        )  # not sure what we do if form_date is None, probably should be an exception
-        self.__symbol_table['mapped.info.form.age'] = form.get_age_in_years(
-            form_date) if form_date else ''
-        self.__symbol_table['mapped.info.form.race'] = form.get_subject_race()
-        self.__symbol_table['mapped.info.form.sex'] = form.get_subject_sex()
-        self.__symbol_table[
-            'mapped.info.form.primlang'] = form.get_primary_language()
+        uds_form = UDSV3Form.create(form)
 
-        form.update_info(self.__symbol_table.get('file.info', {}))
+        self.__symbol_table['file.info'] = uds_form.get_info()
+
+        if uds_form.is_initial_visit():
+            form_date = uds_form.get_form_date()
+            # TODO: not sure what we do if form_date is None,
+            # probably should be an exception
+            self.__symbol_table[
+                'mapped.info.form.age'] = uds_form.get_age_in_years(
+                    form_date) if form_date else ''
+            self.__symbol_table[
+                'mapped.info.form.race'] = uds_form.get_subject_race()
+            self.__symbol_table[
+                'mapped.info.form.sex'] = uds_form.get_subject_sex()
+            self.__symbol_table[
+                'mapped.info.form.primlang'] = uds_form.get_primary_language()
+
+        uds_form.update_info(self.__symbol_table.get('file.info', {}))
+
+    def get_form(self) -> Optional[UDSV3Form]:
+        file_entry = self.get_file_entry()
+        if not file_entry:
+            return None
+
+        form = UDSV3Form(file_entry)
+        if not form.is_form(name='uds'):
+            return None
+
+        return form
 
     def set_latest(self, *, target_key: str, source_key: str,
                    update_date: datetime) -> None:
@@ -119,7 +136,7 @@ class UDSFileCurator(FileCurator):
         version_set.add(f"UDSv{int(float(form_version_value))}")
         self.__symbol_table[subject_version_key] = list(version_set)
 
-    def curate_subject(self, subject: Subject, form: UDSV3Form) -> None:
+    def curate_subject(self, subject: Subject) -> None:
         """Curates subject object info using the assignments to set latest
         value.
 
@@ -128,6 +145,11 @@ class UDSFileCurator(FileCurator):
           assignments: the list of assignments for updates
           date: the date to check for latest assignments
         """
+        form = self.get_form()
+        if not form:
+            return
+
+        self.__symbol_table['subject.info'] = subject.info
 
         self.insert_form_version(
             subject_version_key='subject.info.study-parameters.uds.versions',
@@ -164,31 +186,18 @@ class UDSFileCurator(FileCurator):
 
         subject.update(info=self.__symbol_table.get('subject.info', {}))
 
-    def curate_file(self, file_: Dict[str, Any]) -> None:
-        """Performs curation on the file object and corresponding subject."""
+    # def curate_file(self, file_: Dict[str, Any]) -> None:
+    #     """Performs curation on the file object and corresponding subject."""
 
-        file_entry = self.get_file_entry(file_)
-        subject = self.get_subject(file_entry)
+    #     file_entry = self.get_file_entry(file_)
+    #     subject = self.get_subject(file_entry)
 
-        self.__symbol_table['file.info'] = file_entry.info
-        self.__symbol_table['subject.info'] = subject.info
+    #     self.__symbol_table['file.info'] = file_entry.info
+    #     self.__symbol_table['subject.info'] = subject.info
 
-        form = UDSV3Form(file_entry)
-        if not form.is_form(name='uds'):
-            return
+    #     form = UDSV3Form(file_entry)
+    #     if not form.is_form(name='uds'):
+    #         return
 
-        self.curate_form(form)
-        self.curate_subject(subject=subject, form=form)
-
-    def get_file_entry(self, file_: Dict[str, Any]) -> FileEntry:
-        """Retrieves the FileEntry represented by the dictionary object.
-
-        Args:
-          file_: dictionary object representing FileEntry
-        Returns:
-          the FileEntry corresponding to the dictionary
-        """
-        acq = self.context.get_container_from_ref(file_.get("hierarchy"))
-        filename = self.context.get_input_filename("file-input")
-        file_entry = acq.get_file(filename)
-        return file_entry
+    #     self.curate_form(form)
+    #     self.curate_subject(subject=subject, form=form)
