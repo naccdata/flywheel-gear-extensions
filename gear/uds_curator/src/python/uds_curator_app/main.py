a8fb87ae20ce6c0930577c95bef96fcebbf5829f
Ben Keller <bjkeller@vt.edu>
2025-02-26 00:04:35 +0000

    Update main

--- gear/uds_curator/src/python/uds_curator_app/main.py
+++ gear/uds_curator/src/python/uds_curator_app/main.py
@@ -1,16 +1,21 @@
 """Defines UDS Curator."""
 
 import logging
+from typing import Any, Dict
 
-from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from flywheel.models.file_entry import FileEntry
+from flywheel_gear_toolkit.context.context import GearToolkitContext
+
+from uds_curator_app.uds_curator import UDSFileCurator
 
 log = logging.getLogger(__name__)
 
 
-def run(*, proxy: FlywheelProxy):
+def run(*, context: GearToolkitContext, input_file: Dict[str, Any]) -> None:
     """Runs the UDS Curator process.
 
     Args:
         proxy: the proxy for the Flywheel instance
     """
-    pass
+    curator = UDSFileCurator(context=context, write_report=False)
+    curator.curate_file(input_file)
