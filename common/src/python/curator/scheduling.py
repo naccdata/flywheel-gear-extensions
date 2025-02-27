4ca93310adb0d21d9fbe976a37e1f5fb99c8c885
Ben Keller <bjkeller@vt.edu>
2025-02-27 23:10:35 +0000

    Add subject level processes for project curation

--- common/src/python/curator/scheduling.py
+++ common/src/python/curator/scheduling.py
@@ -1,12 +1,18 @@
+import multiprocessing
+import os
 from datetime import date
-from typing import Dict, List, Optional
+from multiprocessing import Pool
+from typing import Dict, List, Type, TypeVar
 
 from dataview.dataview import ColumnModel, make_builder
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy, ProjectAdaptor
+from flywheel_gear_toolkit.context.context import GearToolkitContext
 from pydantic import BaseModel, ValidationError
 from scheduling.min_heap import MinHeap
 
 from curator.form_curator import FormCurator
+
+C = TypeVar('C', bound=FormCurator)
 
 
 class FileModel(BaseModel):
@@ -39,10 +45,13 @@ class FileModel(BaseModel):
 
 
 class ViewResponseModel(BaseModel):
+    """Defines the data model for a dataview response."""
     data: List[FileModel]
 
 
 class ProjectFormCurator:
+    """Defines a curator for applying a FormCurator to the files in a project.
+    """
 
     def __init__(self, proxy: FlywheelProxy,
                  heap_map: Dict[str, MinHeap[FileModel]]) -> None:
@@ -50,7 +59,16 @@ class ProjectFormCurator:
         self.__heap_map = heap_map
 
     @classmethod
-    def create(cls, project: ProjectAdaptor) -> Optional['ProjectFormCurator']:
+    def create(cls, project: ProjectAdaptor) -> 'ProjectFormCurator':
+        """Creates a ProjectFormCurator for the projects.
+        
+        Pulls information for all of the files in the project.
+        
+        Args:
+          project: the project
+        Returns:
+          the ProjectFormCurator for the form files in the project
+        """
         builder = make_builder(
             label='form-curation-scheduling',
             description='Lists form files for curation',
@@ -90,17 +108,55 @@ class ProjectFormCurator:
         return ProjectFormCurator(proxy=project.proxy,
                                   heap_map=subject_heap_map)
 
-    def apply(self, curator: FormCurator) -> None:
-        # iterate over heaps for individual subjects and curate files
-        # potential namespace conflicts are at subject level
-        for heap in self.__heap_map.values():
-            # this could be an independent process
+    def __compute_cores(self) -> int:
+        """Attempts to compute the number of cores available for processes.
+
+        Apparently, sometimes multiprocessing.cpu_count() returns the
+        number of active cores as opposed to the actual cores. Stack
+        Overflow claims the solution on Linux is to query the process,
+        which needs to pid. So, just asking the os, and multiprocessing.
+        """
+        os_cpu_count = os.cpu_count()
+        os_cpu_cores: int = os_cpu_count if os_cpu_count else 1
+        return max(1, max(os_cpu_cores - 1, multiprocessing.cpu_count() - 1))
+
+    def apply(self, curator_type: Type[C],
+              context: GearToolkitContext) -> None:
+        """Applies a FormCurator to the form files in this curator.
+
+        Builds a curator of the type given with the context to avoid shared
+        state across curators.
+
+        Args:
+          curator_type: a FormCurator subclass
+          context: the gear context
+        """
+
+        def curate_subject(heap: MinHeap[FileModel]) -> None:
+            """Defines a task function for curating the files captured in the
+            heap. 
+
+            Uses the context of the outer apply method to create the curator.
+            
+            Assumes the files are all under the sample user.
+            
+            Args:
+              heap: the min heap of file model objects
+            """
             while len(heap) > 0:
                 file_info = heap.pop()
                 if not file_info:
                     continue
                 file_entry = self.__proxy.get_file(file_info.file_id)
+                curator = curator_type(context=context)
                 curator.curate_container(file_entry)
+
+        process_count = max(4, self.__compute_cores())
+        with Pool(processes=process_count) as pool:
+            for heap in self.__heap_map.values():
+                pool.apply_async(curate_subject, (heap, ))
+            pool.close()
+            pool.join()
 
 
 class ProjectCurationError(Exception):
