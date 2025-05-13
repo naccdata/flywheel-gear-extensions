76997345cc8fa5fcea99e82e7ef89fd12e375782
Chandima H <hewanadun@users.noreply.github.com>
2025-05-13 15:40:53 +0000

    strip whitespace in header

--- gear/form_screening/src/python/form_screening_app/format.py
+++ gear/form_screening/src/python/form_screening_app/format.py
@@ -57,7 +57,7 @@ class CSVFormatterVisitor(CSVVisitor):
         """
 
         out_row = {
-            key.lower(): value
+            key.strip().lower(): value
             for key, value in row.items() if key.lower() not in REDCapKeys
         }
 
