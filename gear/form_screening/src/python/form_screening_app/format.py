4855bb58b843bac4e3773689c89ae63f5965ce76
Ben Keller <bjkeller@vt.edu>
2025-07-21 14:53:03 +0000

    Move error message into csv formatter visitor and change to raise csv visitor error

--- gear/form_screening/src/python/form_screening_app/format.py
+++ gear/form_screening/src/python/form_screening_app/format.py
@@ -2,7 +2,7 @@
 
 from typing import Any, Dict, List, Optional, TextIO
 
-from inputs.csv_reader import CSVVisitor, MalformedFileError
+from inputs.csv_reader import CSVVisitor, CSVVisitorError
 from keys.keys import REDCapKeys
 from outputs.errors import ErrorWriter, malformed_file_error
 from outputs.outputs import CSVWriter
@@ -117,7 +117,8 @@ class CSVFormatterVisitor(CSVVisitor):
                 f"Number of columns in line {line_num} "
                 f"do not match with the number of columns in the header row"
             )
-            raise MalformedFileError(message)
+            self.__error_writer.write(malformed_file_error(message))
+            raise CSVVisitorError(message)
 
         out_row = {
             key.strip().lower(): value
