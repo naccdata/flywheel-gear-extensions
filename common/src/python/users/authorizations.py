2d81f2dbc2b45c9673e4a465215af098f91d58ee
Ben Keller <bjkeller@vt.edu>
2025-08-29 18:31:42 +0000

    Refactor authorizations

--- common/src/python/users/authorizations.py
+++ common/src/python/users/authorizations.py
@@ -1,20 +1,104 @@
 """Defines components related to user authorizations."""
 
-from typing import Dict, Literal, Set
+from typing import Any, Dict, Literal, Optional, Self, get_args
 
-from keys.types import DatatypeNameType
-from pydantic import BaseModel, model_serializer
+from flywheel.models.role_output import RoleOutput
+from keys.types import DatatypeNameType, PipelineStageType
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    ModelWrapValidatorHandler,
+    ValidationError,
+    ValidationInfo,
+    model_serializer,
+    model_validator,
+)
 
 ActionType = Literal["submit-audit", "view"]
 
 
+class PipelineLabel(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    pipeline: PipelineStageType
+    datatype: Optional[DatatypeNameType] = None
+    study_id: str = "adrc"
+
+    @model_serializer
+    def string_pipeline_label(self) -> str:
+        result = f"{self.pipeline}"
+        result = f"{result}-{self.datatype}" if self.datatype else result
+        result = f"{result}-{self.study_id}" if self.study_id != "adrc" else result
+        return result
+
+    @model_validator(mode="wrap")
+    @classmethod
+    def string_validator(
+        cls, label: Any, handler: ModelWrapValidatorHandler[Self]
+    ) -> Self:
+        if isinstance(label, PipelineLabel):
+            return handler(label)
+        if isinstance(label, dict):
+            return handler(label)
+        if not isinstance(label, str):
+            raise TypeError(f"Unexpected type for pipeline label: {type(label)}")
+
+        tokens = label.split("-")
+        if not tokens:
+            raise TypeError("Empty pipeline label")
+
+        study_id = "adrc"
+        datatype = None
+        pipeline = tokens[0]
+
+        if len(tokens) > 1:
+            datatype = "-".join(tokens[1:])
+            if datatype not in get_args(DatatypeNameType):
+                datatype = "-".join(tokens[1:-1])
+                study_id = tokens[-1:][0]
+
+        datatype = datatype if datatype else None
+        return handler(
+            {"pipeline": pipeline, "datatype": datatype, "study_id": study_id}
+        )
+
+
 class Activity(BaseModel):
-    data: DatatypeNameType
+    """Data model representing an user activity for authorization.
+
+    Consists of an action and datatype.
+    """
+
+    model_config = ConfigDict(frozen=True)
+
+    datatype: DatatypeNameType
     action: ActionType
 
     @model_serializer
     def string_activity(self) -> str:
-        return f"{self.action}-{self.data}"
+        """Serializes this activity as a string of the form action-datatype.
+
+        Returns:
+          string representation of activity
+        """
+        return f"{self.action}-{self.datatype}"
+
+    @model_validator(mode="wrap")
+    @classmethod
+    def string_validator(
+        cls, activity: Any, handler: ModelWrapValidatorHandler[Self]
+    ) -> Self:
+        if isinstance(activity, Activity):
+            return handler(activity)
+        if isinstance(activity, dict):
+            return handler(activity)
+        if not isinstance(activity, str):
+            raise TypeError(f"Unexpected type for activity: {type(activity)}")
+
+        tokens = activity.split("-")
+        datatype = tokens[-1:].pop()
+        action_string = "-".join(tokens[:-1])
+        return handler({"datatype": datatype, "action": action_string})
 
 
 class StudyAuthorizations(BaseModel):
@@ -30,7 +114,20 @@ class StudyAuthorizations(BaseModel):
           datatype: the datatype
           action: the action
         """
-        self.activities[datatype] = Activity(data=datatype, action=action)
+        self.activities[datatype] = Activity(datatype=datatype, action=action)
+
+    def __contains__(self, activity_name: str) -> bool:
+        try:
+            input_activity = Activity.model_validate(activity_name)
+        except ValidationError:
+            # TODO: needs to raise error
+            return False
+
+        activity = self.activities.get(input_activity.datatype)
+        if activity is None:
+            return False
+
+        return input_activity.action == activity.action
 
 
 class AuthMap(BaseModel):
@@ -39,17 +136,33 @@ class AuthMap(BaseModel):
     Represents table as project label -> activity -> role.
     """
 
-    project_authorizations: Dict[str, Dict[str, str]]
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    project_authorizations: Dict[str, Dict[Activity, RoleOutput]]
+
+    def __get_roles(
+        self, label: str, authorizations: StudyAuthorizations
+    ) -> list[RoleOutput]:
+        role_map: dict[str, RoleOutput] = {}
+        activity_map = self.project_authorizations.get(label, {})
+        for activity in authorizations.activities.values():
+            role = activity_map.get(activity)
+            if role:
+                role_map[role.label] = role
+        return list(role_map.values())
 
     def get(
         self, *, project_label: str, authorizations: StudyAuthorizations
-    ) -> Set[str]:
+    ) -> list[RoleOutput]:
         """Gets the roles for a project and authorizations.
 
-        Matches project label against the authorization keys.
-        If the label has a study suffix, e.g. "ingest-form-dvcid", will first
-        check the full label, and if that fails will remove the suffix and
-        retry (e.g., "ingest-form").
+        Matches project label against the project keys of this map.
+        If the label is either "center-portal" or "metadata", looks up roles.
+        Otherwise, assumes a pipeline project where label may have a study
+        suffix, e.g. "ingest-form-dvcid".
+        So, first checks the full label, and if that fails, removes the suffix and
+        retries (e.g., "ingest-form").
+        This allows the authorization data to have study-specific role mappings.
 
         Args:
             project_id: the project ID
@@ -57,7 +170,8 @@ class AuthMap(BaseModel):
         Returns:
             The list of roles
         """
-        roles: Set[str] = set()
+        if project_label in {"center-portal", "metadata"}:
+            return self.__get_roles(label=project_label, authorizations=authorizations)
 
         pipeline_label = project_label
         if pipeline_label not in self.project_authorizations:
@@ -65,12 +179,63 @@ class AuthMap(BaseModel):
             pipeline_label = "-".join(project_label.split("-")[:-1])
 
         if pipeline_label not in self.project_authorizations:
-            return roles
+            return []
 
-        activity_map = self.project_authorizations.get(pipeline_label, {})
-        for activity in authorizations.activities:
-            role_name = activity_map.get(activity)
-            if role_name:
-                roles.add(role_name)
+        return self.__get_roles(label=pipeline_label, authorizations=authorizations)
 
-        return roles
+    @model_validator(mode="wrap")
+    @classmethod
+    def create_auth_map(
+        cls,
+        auth_object: Any,
+        handler: ModelWrapValidatorHandler[Self],
+        info: ValidationInfo,
+    ) -> Self:
+        """Creates an AuthMap object for the input auth map object where roles
+        are represented as strings.
+
+        Note: does not require that auth_object has the "project_authorizations"
+        key, which simplifies loading the config file.
+
+        Args:
+          auth_object: the authorization map as a dictionary of strings
+          role_map: the map of role name to role
+        Raises:
+          TypeError if the auth_object is not structured correctly
+          ValidationError if activity names do not parse as Activity objects
+        """
+        if isinstance(auth_object, AuthMap):
+            return handler(auth_object)
+        if not isinstance(auth_object, dict):
+            raise TypeError("expected dictionary for authmap")
+
+        if not isinstance(info.context, dict):
+            raise TypeError("role map is required")
+        role_map = info.context.get("role_map", {})
+
+        project_auth = auth_object.get("project_authorizations")
+        project_auth = project_auth if project_auth else auth_object
+        if not isinstance(project_auth, dict):
+            raise TypeError('Expecting "project_authorizations" to be a dict')
+
+        auth_dict = {}
+        for project_label, role_assignment in project_auth.items():
+            if not isinstance(role_assignment, dict):
+                raise TypeError(
+                    f"Expecting role assignment for project {project_label}"
+                )
+
+            project_dict = {}
+            for activity_name, role_name in role_assignment.items():
+                activity = Activity.model_validate(activity_name)
+                role = role_map.get(role_name)
+                if not role:
+                    raise TypeError(
+                        "No matching role for "
+                        f"{project_label}:{activity_name}:{role_name}"
+                    )
+                project_dict[activity] = role
+
+            auth_dict[project_label] = project_dict
+
+        return handler({"project_authorizations": auth_dict})
