b6984b38b36fba47f59f2e485eb20e83c31689de
Ben Keller <bjkeller@vt.edu>
2025-08-29 18:32:24 +0000

    Update types

--- common/src/python/keys/types.py
+++ common/src/python/keys/types.py
@@ -1,4 +1,16 @@
 from typing import Literal
 
-DatatypeNameType = Literal["apoe", "biomarker", "dicom", "enrollment", "form", "gwas"]
+DatatypeNameType = Literal[
+    "apoe",
+    "biomarker",
+    "dicom",
+    "enrollment",
+    "form",
+    "genetic-availability",
+    "gwas",
+    "imputation",
+]
 ModuleName = Literal["UDS", "FTLD", "LBD", "MDS"]
+PipelineStageType = Literal[
+    "ingest", "retrospective", "sandbox", "distribution", "accepted"
+]
