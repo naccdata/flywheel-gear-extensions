a8bd4605d233773d5f359069246e250afdbe71d9
Ben Keller <bjkeller@vt.edu>
2025-08-29 18:33:36 +0000

    Update authorization tests for changes

--- common/test/python/users/test_authorizations.py
+++ common/test/python/users/test_authorizations.py
@@ -2,9 +2,12 @@
 
 import pytest
 import yaml
+from flywheel.models.role_output import RoleOutput
 from pydantic import ValidationError
 from users.authorizations import (
+    Activity,
     AuthMap,
+    PipelineLabel,
     StudyAuthorizations,
 )
 
@@ -16,10 +19,18 @@ def empty_auth():
 
 
 @pytest.fixture
-def auth_map_alpha():
+def role_map():
+    yield {
+        "read-only": RoleOutput(label="read-only"),
+        "upload": RoleOutput(label="upload"),
+    }
+
+
+@pytest.fixture
+def auth_map_alpha(role_map):
     """AuthMap object."""
-    auth_map = AuthMap(
-        project_authorizations={
+    auth_map = AuthMap.model_validate(
+        {
             "accepted": {
                 "submit-audit-form": "read-only",
                 "view-form": "read-only",
@@ -34,7 +45,8 @@ def auth_map_alpha():
                 "submit-audit-enrollment": "upload",
             },
             "sandbox-form": {"submit-audit-form": "upload"},
-        }
+        },
+        context={"role_map": role_map},
     )
     yield auth_map
 
@@ -82,8 +94,8 @@ class TestAuthMap:
 
     def test_empty_map(self, empty_auth: StudyAuthorizations):
         """Test empty map."""
-        auth_map = AuthMap(project_authorizations={})
-        assert auth_map.get(project_label="dummy", authorizations=empty_auth) == set()
+        auth_map = AuthMap.model_validate({}, context={"role_map": {}})
+        assert auth_map.get(project_label="dummy", authorizations=empty_auth) == []
 
     def test_authmap(
         self,
@@ -92,47 +104,149 @@ class TestAuthMap:
         auth_map_alpha: AuthMap,
     ):
         """Test authmap."""
-        assert auth_map_alpha.get(
+        role_list = auth_map_alpha.get(
             project_label="accepted", authorizations=alpha_authorizations
-        ) == {"read-only"}
-        assert auth_map_alpha.get(
+        )
+        assert [role.label for role in role_list] == ["read-only"]
+        role_list = auth_map_alpha.get(
             project_label="ingest-form", authorizations=alpha_authorizations
-        ) == {"upload"}
+        )
+        assert [role.label for role in role_list] == ["upload"]
         assert (
             auth_map_alpha.get(
                 project_label="ingest-dicom", authorizations=alpha_authorizations
             )
-            == set()
+            == []
         )
-        assert auth_map_alpha.get(
+        role_list = auth_map_alpha.get(
             project_label="sandbox-form", authorizations=alpha_authorizations
-        ) == {"upload"}
+        )
+        assert [role.label for role in role_list] == ["upload"]
 
-        assert auth_map_alpha.get(
+        role_list = auth_map_alpha.get(
             project_label="accepted", authorizations=beta_authorizations
-        ) == {"read-only"}
-        assert auth_map_alpha.get(
+        )
+        assert [role.label for role in role_list] == ["read-only"]
+        role_list = auth_map_alpha.get(
             project_label="ingest-form", authorizations=beta_authorizations
-        ) == {"read-only"}
+        )
+        assert [role.label for role in role_list] == ["read-only"]
         assert (
             auth_map_alpha.get(
                 project_label="ingest-dicom", authorizations=beta_authorizations
             )
-            == set()
+            == []
         )
         assert (
             auth_map_alpha.get(
                 project_label="sandbox-form", authorizations=beta_authorizations
             )
-            == set()
+            == []
         )
 
-    def test_yaml(self, auth_map_alpha: AuthMap, auth_map_alpha_yaml: str):
+    def test_yaml(
+        self,
+        auth_map_alpha: AuthMap,
+        auth_map_alpha_yaml: str,
+        role_map: dict[str, RoleOutput],
+    ):
         """Test YAML conversion."""
         yaml_object = yaml.safe_load(auth_map_alpha_yaml)
-        load_map = AuthMap(project_authorizations=yaml_object)
+        load_map = AuthMap.model_validate(yaml_object, context={"role_map": role_map})
         assert load_map == auth_map_alpha
 
         yaml_list = yaml.safe_load("---\n- blah\n- blah\n")
-        with pytest.raises(ValidationError):  # type: ignore
-            AuthMap(project_authorizations=yaml_list)
+        with pytest.raises(TypeError):  # type: ignore
+            AuthMap.model_validate(yaml_list, context={"role_map": role_map})
+
+
+class TestActivity:
+    def test_serialization(self):
+        activity = Activity(datatype="form", action="submit-audit")
+
+        activity_name = activity.model_dump()
+        assert activity_name == "submit-audit-form"
+
+        try:
+            activity_load = Activity.model_validate(activity_name)
+            assert activity_load == activity
+        except ValidationError as error:
+            raise AssertionError(error) from error
+
+        activity = Activity(datatype="form", action="view")
+
+        activity_name = activity.model_dump()
+        assert activity_name == "view-form"
+
+        try:
+            activity_load = Activity.model_validate(activity_name)
+            assert activity_load == activity
+        except ValidationError as error:
+            raise AssertionError(error) from error
+
+    def test_invalid(self):
+        with pytest.raises(ValidationError) as info:
+            Activity.model_validate({"datatype": "junk", "action": "view"})
+        assert len(info.value.errors()) == 1
+        error = info.value.errors()[0]
+        assert error["loc"][0] == "datatype"
+
+        with pytest.raises(ValidationError) as info:
+            Activity.model_validate({"datatype": "form", "action": "junk"})
+        assert len(info.value.errors()) == 1
+        error = info.value.errors()[0]
+        assert error["loc"][0] == "action"
+
+
+class TestAuthorization:
+    def test_contains(self):
+        authorization = StudyAuthorizations(study_id="dummy")
+        authorization.add(datatype="form", action="submit-audit")
+
+        assert "submit-audit-form" in authorization
+        assert "view-form" not in authorization
+
+
+class TestPipelineLabel:
+    def test_valid(self):
+        label_object = PipelineLabel(
+            pipeline="distribution", datatype="genetic-availability", study_id="dummy"
+        )
+        label_string = label_object.model_dump()
+        assert label_string == "distribution-genetic-availability-dummy"
+        label_load = PipelineLabel.model_validate(label_string)
+        assert label_object == label_load
+
+        label_object = PipelineLabel(
+            pipeline="distribution", datatype="genetic-availability"
+        )
+        label_string = label_object.model_dump()
+        assert label_string == "distribution-genetic-availability"
+        label_load = PipelineLabel.model_validate(label_string)
+        assert label_object == label_load
+
+        label_object = PipelineLabel(
+            pipeline="distribution", datatype="form", study_id="dummy"
+        )
+        label_string = label_object.model_dump()
+        assert label_string == "distribution-form-dummy"
+        label_load = PipelineLabel.model_validate(label_string)
+        assert label_object == label_load
+
+        label_object = PipelineLabel(pipeline="distribution", datatype="form")
+        label_string = label_object.model_dump()
+        assert label_string == "distribution-form"
+        label_load = PipelineLabel.model_validate(label_string)
+        assert label_object == label_load
+
+        label_object = PipelineLabel(pipeline="accepted", study_id="dummy")
+        label_string = label_object.model_dump()
+        assert label_string == "accepted-dummy"
+        label_load = PipelineLabel.model_validate(label_string)
+        assert label_object == label_load
+
+        label_object = PipelineLabel(pipeline="accepted")
+        label_string = label_object.model_dump()
+        assert label_string == "accepted"
+        label_load = PipelineLabel.model_validate(label_string)
+        assert label_object == label_load
