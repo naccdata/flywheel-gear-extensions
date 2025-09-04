8d708f1631b51e663a5a28f8bded310cd63282c4
Emily Cheng <echeng06@uw.edu>
2025-09-03 19:29:37 -0700

    Remove retry, add 1 second sleep every 1000 records

--- common/src/python/utils/decorators.py
+++ common/src/python/utils/decorators.py
@@ -28,29 +28,3 @@ def api_retry(func, max_retries: int = 3):
                     raise e
 
     return wrapper
-
-
-def sql_connection_retry(func, max_retries: int = 3, wait_time: int = 10):
-    """Decorator to handle 1040, 'Too many connections' error."""
-
-    def wrapper(*args, **kwargs):
-        retries = 0
-        while retries <= max_retries:
-            try:
-                return func(*args, **kwargs)
-                break
-            except pymysql.err.OperationalError as e:
-                # only retry on 1040
-                if e.args[0] == 1040:
-                    retries += 1
-                    if retries <= max_retries:
-                        log.warning(
-                            "Too many SQL connections, retrying "
-                            f"{retries}/{max_retries} in {wait_time} seconds"
-                        )
-                        time.sleep(wait_time)
-                        continue
-
-                raise e
-
-    return wrapper
