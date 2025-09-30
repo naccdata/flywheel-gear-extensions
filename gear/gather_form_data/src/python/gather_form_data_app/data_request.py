92736e0e05d6be0ab32a51a849a421c6adc04d38
Ben Keller <bjkeller@vt.edu>
2025-09-30 15:11:07 +0000

    Define data request visitor

--- gear/gather_form_data/src/python/gather_form_data_app/data_request.py
+++ gear/gather_form_data/src/python/gather_form_data_app/data_request.py
@@ -1,27 +1,48 @@
 import re
 from typing import Any, Dict, List
-from pydantic import BaseModel, Field
 
-from centers.nacc_group import NACCGroup
-from flywheel_adaptor.flywheel_proxy import ProjectAdaptor
+from flywheel_adaptor.flywheel_proxy import FlywheelProxy
 from identifiers.model import NACCID_PATTERN
 from inputs.csv_reader import CSVVisitor
+from outputs.error_models import CSVLocation, FileError
 from outputs.error_writer import ErrorWriter
-from outputs.errors import missing_field_error
+from outputs.errors import malformed_file_error, missing_field_error
+from pydantic import BaseModel, Field, ValidationError
 
 
 class DataRequest(BaseModel):
     """Data model for a row of a data request file."""
+
     naccid: str = Field(max_length=10, pattern=NACCID_PATTERN)
-    study: str
 
-class RequestClusteringVisitor(CSVVisitor):
 
-    def __init__(self, admin_group: NACCGroup, error_writer: ErrorWriter, project_names: list[str]) -> None:
-        self.__admin_group = admin_group
+class DataRequestMatch(BaseModel):
+    """Data model representing a participant matching data request for
+    NACCID."""
+
+    naccid: str
+    subject_id: str
+    project_label: str
+
+
+class DataRequestLookupVisitor(CSVVisitor):
+    """Gathers subject matches for a data request file given as a CSV file
+    where each row loads as a DataRequest object."""
+
+    def __init__(
+        self,
+        proxy: FlywheelProxy,
+        error_writer: ErrorWriter,
+        project_names: list[str],
+        study_id: str,
+    ) -> None:
+        self.__proxy = proxy
         self.__error_writer = error_writer
-        self.__pipeline_map: dict[int, dict[str, ProjectAdaptor]]
-        self.__project_names = project_names
+        self.__expected_studies = {study_id, "adrc"}
+        self.__project_names = list(project_names)
+        self.__project_names.extend({f"{name}-{study_id}" for name in project_names})
+        self.__project_matcher = re.compile(f"^{'|'.join(self.__project_names)}$")
+        self.subjects: list[DataRequestMatch] = []
 
     def visit_header(self, header: List[str]) -> bool:
         """Checks that the header has ADCID, PTID and study keys.
@@ -38,26 +59,50 @@ class RequestClusteringVisitor(CSVVisitor):
 
         return True
 
-    def __load_pipeline(self, adcid: int) -> None:
-        if adcid in self.__pipeline_map:
-            return
+    def __get_matches(self, request: DataRequest) -> list[DataRequestMatch]:
+        """Returns list of subject IDs matching the NACCID in the request that
+        have projects matching the study and name constraints.
 
-        project_map: dict[str, ProjectAdaptor] = {}
-        projects = self.__admin_group.get_pipeline(adcid)
+        Args:
+          request: the data request
+        Returns:
+          a list of subject IDs
+        """
+        result = []
+        subjects = self.__proxy.get_subject_by_label(request.naccid)
+        for subject in subjects:
+            parent_project = self.__proxy.get_container_by_id(subject.parents.project)
+            if self.__project_matcher.match(parent_project.label):
+                result.append(
+                    DataRequestMatch(
+                        naccid=request.naccid,
+                        subject_id=subject.id,
+                        project_label=parent_project.label,
+                    )
+                )
 
-        pattern = f"^{'|'.join(self.__project_names)}$"
-        matcher = re.compile(pattern)
-        project_map = {project.label: project for project in projects if matcher.match(project.label)}
-        if not project_map:
-            return
+        return result
 
-        self.__pipeline_map[adcid] = project_map
-
-    def __get_projects(self, adcid: int) -> list[ProjectAdaptor]:
-        self.__load_pipeline(adcid)
-
-        return []
-
-    
     def visit_row(self, row: Dict[str, Any], line_num: int) -> bool:
-        return super().visit_row(row, line_num)
+        try:
+            query = DataRequest.model_validate(row)
+        except ValidationError as error:
+            self.__error_writer.write(malformed_file_error(str(error)))
+
+        matching_subjects = self.__get_matches(request=query)
+        if not matching_subjects:
+            self.__error_writer.write(
+                FileError(
+                    error_code="no-participant",  # pyright: ignore[reportCallIssue]
+                    error_type="error",  # pyright: ignore[reportCallIssue]
+                    location=CSVLocation(line=line_num, column_name="naccid"),
+                    message=(
+                        f"no participant {query.naccid} with data for "
+                        f"{','.join(self.__expected_studies)}"
+                    ),
+                )
+            )
+            return True  # ignore row
+
+        self.subjects.extend(matching_subjects)
+        return True
