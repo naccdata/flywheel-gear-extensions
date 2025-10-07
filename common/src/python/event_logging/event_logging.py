cdb20c7b3236b9c6f591e07ff03c6b88751c4b9a
Ben Keller <bjkeller@vt.edu>
2025-10-07 16:07:16 +0000

    Fix timestamp in event logging

--- common/src/python/event_logging/event_logging.py
+++ common/src/python/event_logging/event_logging.py
@@ -12,10 +12,10 @@ class VisitEventLogger:
     └── adcid-0
         ├── ingest-dicom
         │   └── log
-        │       └── log000001.json
+        │       └── log-submit-20251007-160112.json
         └── ingest-form
             └── log
-                └── log000001.json
+                └── log-submit-20251003-120100.json
     """
 
     def __init__(self, s3_bucket: S3BucketInterface) -> None:
@@ -28,10 +28,11 @@ class VisitEventLogger:
           event: the visit event
         """
         event_json = event.model_dump_json(exclude_none=True)
+        timestamp = event.timestamp.strftime("%Y%m%d-%H%M%S")
         filename = (
             f"adcid-{event.pipeline_adcid}/"
             f"{event.project_label}/"
             f"log/"
-            f"log-{event.action}-{event.timestamp}.json"
+            f"log-{event.action}-{timestamp}.json"
         )
         self.__bucket.put_file_object(filename=filename, contents=event_json)
