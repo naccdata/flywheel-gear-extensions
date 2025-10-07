8ed71aa9fef12d616ffbcdd50842c3054595f312
Ben Keller <bjkeller@vt.edu>
2025-10-07 17:51:57 +0000

    Update vist event tests

--- common/test/python/event_logging/test_visit_events.py
+++ common/test/python/event_logging/test_visit_events.py
@@ -1,8 +1,6 @@
 from datetime import date, datetime
 
-from pydantic import ValidationError
 import pytest
-
 from event_logging.visit_events import VisitEvent
 
 
@@ -13,7 +11,7 @@ class TestVisitEvent:
                 action="submit",
                 pipeline_adcid=0,
                 project_label="ingest-form",
-                center_label= "sample-center",
+                center_label="sample-center",
                 ptid="dummy",
                 visit_date=date(year=2025, month=10, day=7),
                 visit_number="v1",
@@ -28,15 +26,15 @@ class TestVisitEvent:
 
         with pytest.raises(ValueError):
             VisitEvent(
-            action="submit",
-            pipeline_adcid=0,
-            project_label="ingest-dicom",
-            center_label= "sample-center",
-            ptid="dummy",
-            visit_date=date(year=2025, month=10, day=7),
-            visit_number="v1",
-            datatype="dicom",
-            module="UDS",
-            timestamp=datetime.now(),
-            gear_name="dummy-gear",
-        )
+                action="submit",
+                pipeline_adcid=0,
+                project_label="ingest-dicom",
+                center_label="sample-center",
+                ptid="dummy",
+                visit_date=date(year=2025, month=10, day=7),
+                visit_number="v1",
+                datatype="dicom",
+                module="UDS",
+                timestamp=datetime.now(),
+                gear_name="dummy-gear",
+            )
