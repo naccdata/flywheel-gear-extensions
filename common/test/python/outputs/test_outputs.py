fb3dfce93cf0f1a7bb9a192e7f2b560648502caf
Ben Keller <bjkeller@vt.edu>
2025-10-09 21:26:06 +0000

    add test for string csv writer

--- common/test/python/outputs/test_outputs.py
+++ common/test/python/outputs/test_outputs.py
@@ -2,9 +2,8 @@ from outputs.outputs import StringCSVWriter
 
 
 class TestStringCSVWriter:
-
     def test_writer(self):
         writer = StringCSVWriter()
         writer.write({"alpha": 1, "beta": "one"})
         content = writer.get_content()
-        assert content == 'alpha,beta\n1,one\n'
+        assert content == "alpha,beta\n1,one\n"
