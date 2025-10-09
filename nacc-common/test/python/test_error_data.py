2b590089e88114839f63e7129bfc7e4478c5760a
Ben Keller <bjkeller@vt.edu>
2025-10-09 18:18:18 +0000

    Add a sanity check on error_data header constants

---
+++ nacc-common/test/python/test_error_data.py
@@ -0,0 +1,15 @@
+from error_data import ERROR_HEADER_NAMES, STATUS_HEADER_NAMES
+from outputs.visit_submission_error import ErrorReportModel
+from outputs.visit_submission_status import StatusReportModel
+
+
+class TestErrorData:
+    def test_error_header_names(self):
+        assert ERROR_HEADER_NAMES is not None
+        assert ERROR_HEADER_NAMES != []
+        assert ErrorReportModel.serialized_fieldnames() == ERROR_HEADER_NAMES
+
+    def test_status_header_names(self):
+        assert STATUS_HEADER_NAMES is not None
+        assert STATUS_HEADER_NAMES != []
+        assert list(StatusReportModel.model_fields.keys()) == STATUS_HEADER_NAMES
