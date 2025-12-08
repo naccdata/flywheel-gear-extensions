94893cd02f71a9460260262b40b2de9c289a23ed
Ben Keller <bjkeller@vt.edu>
2025-12-08 12:59:36 -0800

    Add devcontainer terminal script

---
+++ bin/terminal.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+if ! command -v devcontainer &> /dev/null; then
+    echo "Error: devcontainer command not found"
+    echo "Please install the devcontainer CLI: npm install -g @devcontainers/cli"
+    exit 1
+fi
+
+export WORKSPACE_FOLDER=`pwd`
+export CONTAINER_HOST=`devcontainer exec --workspace-folder $WORKSPACE_FOLDER hostname`
+export WORKSPACE=`basename $WORKSPACE_FOLDER`
+docker exec -u vscode -w /workspaces/$WORKSPACE -ti $CONTAINER_HOST /bin/zsh
