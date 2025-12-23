54383fd7ca289728cfab20c4ef2ccf93b3a0c5e3
Ben Keller <bjkeller@vt.edu>
2025-12-23 15:15:56 +0000

    Pull enrollment transfer changes from feature/add-event-logging

--- common/src/python/enrollment/enrollment_transfer.py
+++ common/src/python/enrollment/enrollment_transfer.py
@@ -2,7 +2,6 @@
 form."""
 
 import logging
-import re
 from datetime import datetime
 from typing import Any, Dict, Literal, Optional
 
@@ -21,14 +20,11 @@ from identifiers.model import (
     OptionalNACCIDField,
 )
 from inputs.csv_reader import RowValidator
-from keys.keys import SysErrorCodes
-from nacc_common.error_models import VisitKeys
 from nacc_common.field_names import FieldNames
 from outputs.error_writer import ErrorWriter
 from outputs.errors import (
     existing_participant_error,
     identifier_error,
-    preprocessing_error,
 )
 from pydantic import BaseModel, Field, field_validator
 
@@ -352,64 +348,6 @@ class NewGUIDRowValidator(RowValidator):
 #           True if no existing participant matches, False otherwise
 #         """
 #         return True
-
-
-# pylint: disable=(too-few-public-methods)
-class CenterValidator(RowValidator):
-    """Row validator to check whether the row has the correct ADCID and the
-    PTID matches expected format."""
-
-    def __init__(
-        self, center_id: int, date_field: str, error_writer: ErrorWriter
-    ) -> None:
-        self.__center_id = center_id
-        self.__date_field = date_field
-        self.__error_writer = error_writer
-
-    def check(self, row: Dict[str, Any], line_number: int) -> bool:
-        """Checks that the row has the expected ADCID and the PTID matches
-        expected format.
-
-        Args:
-          row: the dictionary for the row
-          line_number: the line number of the row
-
-        Returns:
-          True if the ADCID matches and PTID in expected format, False otherwise.
-        """
-
-        valid = True
-        if str(row.get(FieldNames.ADCID)) != str(self.__center_id):
-            log.error("Center ID for project must match form ADCID")
-            self.__error_writer.write(
-                preprocessing_error(
-                    field=FieldNames.ADCID,
-                    value=row[FieldNames.ADCID],
-                    line=line_number,
-                    error_code=SysErrorCodes.ADCID_MISMATCH,
-                    visit_keys=VisitKeys.create_from(
-                        record=row, date_field=self.__date_field
-                    ),
-                )
-            )
-            valid = False
-
-        ptid = row.get(FieldNames.PTID, "")
-        if not re.fullmatch(PTID_PATTERN, ptid):
-            self.__error_writer.write(
-                preprocessing_error(
-                    field=FieldNames.PTID,
-                    value=ptid,
-                    line=line_number,
-                    error_code=SysErrorCodes.INVALID_PTID,
-                    visit_keys=VisitKeys.create_from(
-                        record=row, date_field=self.__date_field
-                    ),
-                )
-            )
-            valid = False
-
-        return valid
 
 
 class EnrollmentError(Exception):
