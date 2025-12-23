fa5410874bdeba16ca2d766f30ab08599c619e8a
Ben Keller <bjkeller@vt.edu>
2025-12-22 16:27:54 -0800

    Delete old error logger

--- gear/identifier_lookup/src/python/identifier_app/main.py
+++ gear/identifier_lookup/src/python/identifier_app/main.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List, Optional, TextIO
 
 from configs.ingest_configs import ErrorLogTemplate, ModuleConfigs
 from enrollment.enrollment_transfer import CenterValidator
+from error_logging.error_logger import update_error_log_and_qc_metadata
 from flywheel_adaptor.flywheel_proxy import ProjectAdaptor
 from gear_execution.gear_execution import GearExecutionError
 from identifiers.identifiers_repository import (
@@ -16,7 +17,6 @@ from identifiers.model import IdentifierObject, clean_ptid
 from inputs.csv_reader import CSVVisitor, read_csv
 from nacc_common.error_models import FileError, VisitKeys
 from nacc_common.field_names import FieldNames
-from outputs.error_logger import update_error_log_and_qc_metadata
 from outputs.error_writer import ListErrorWriter
 from outputs.errors import (
     identifier_error,
