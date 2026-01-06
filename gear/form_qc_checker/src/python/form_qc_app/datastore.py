54a7a1549db7ff0928febf70ce14104165f2f56a
Emily Cheng <echeng06@uw.edu>
2026-01-06 15:22:30 -0800

    Fix legacy qc gear issue

--- gear/form_qc_checker/src/python/form_qc_app/datastore.py
+++ gear/form_qc_checker/src/python/form_qc_app/datastore.py
@@ -309,7 +309,7 @@ class DatastoreHelper(Datastore):
                 search_col=FieldNames.PACKET,
                 search_val=ivp_codes,
                 search_op=DefaultValues.FW_SEARCH_OR,
-                qc_gear=DefaultValues.QC_GEAR,
+                qc_gear=DefaultValues.LEGACY_QC_GEAR,
                 extra_columns=[date_field],
             )
 
@@ -367,7 +367,7 @@ class DatastoreHelper(Datastore):
                 search_col=FieldNames.PACKET,
                 search_val=ivp_codes,
                 search_op=DefaultValues.FW_SEARCH_OR,
-                qc_gear=DefaultValues.QC_GEAR,
+                qc_gear=DefaultValues.LEGACY_QC_GEAR,
                 extra_columns=[date_field],
             )
 
