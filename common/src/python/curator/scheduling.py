0b22fe2abd6f9c74f69312a0a6a50585ca2ab605
Emily Cheng <echeng06@uw.edu>
2026-05-14 09:20:23 -0700

    Fix to min

--- common/src/python/curator/scheduling.py
+++ common/src/python/curator/scheduling.py
@@ -161,7 +161,7 @@ class ProjectCurationScheduler:
           exclude_subjects: Subjects to exclude in
           max_num_workers: max number of workers to use
         """
-        process_count = max(max_num_workers, self.__compute_cores())
+        process_count = min(max_num_workers, self.__compute_cores())
         log.info(f"Using {process_count} workers")
 
         results = []
