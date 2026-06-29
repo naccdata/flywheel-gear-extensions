9ddc8a600110bff304d8513cd9727e923fddbbde
David Hunt <davidoh@uw.edu>
2026-06-29 08:14:52 -0700

    refactor Flywheel-side information collection into class FlywheelREDCapImageForm to be reused with redcap-image-form-importer gear

--- gear/redcap_image_form_creator/src/python/redcap_image_form_creator_app/main.py
+++ gear/redcap_image_form_creator/src/python/redcap_image_form_creator_app/main.py
@@ -3,23 +3,30 @@
 import json
 import logging
 import re
+import sys
 from time import sleep
-from typing import Any, Dict, NoReturn, Optional
+from typing import NoReturn, Optional
 
 from flywheel.models.container_output import ContainerOutput
-from flywheel.models.file_entry import FileEntry
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from flywheel_adaptor.flywheel_redcap_image_form import FlywheelREDCapImageForm
 from gear_execution.gear_execution import GearExecutionError
 from redcap_api.redcap_connection import REDCapConnection
 from redcap_api.redcap_project import REDCapProject
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 pass_tag = "redcap-image-form-creator-PASS"
 fail_tag = "redcap-image-form-creator-FAIL"
 
-
 def tag_pass(session: ContainerOutput) -> None:
+    """Handles the gear's tagging when it has completed successfully
+
+    Args:
+        session: target Flywheel session
+    """
+
     if fail_tag in session.tags:
         session.delete_tag(fail_tag)
     if pass_tag not in session.tags:
@@ -27,6 +34,17 @@ def tag_pass(session: ContainerOutput) -> None:
 
 
 def tag_fail(dry_run: bool, session: ContainerOutput, msg: str) -> NoReturn:
+    """Handles gear-related tagging upon failure and raises an error.
+
+    Args:
+        dry_run: flag for dry run (data collected but no modifications)
+        session: target Flywheel session
+        msg: string that describes the failure reason
+
+    Raises:
+        GearExecutionError because the gear has failed
+    """
+
     if not dry_run:
         if pass_tag in session.tags:
             session.delete_tag(pass_tag)
@@ -34,180 +52,19 @@ def tag_fail(dry_run: bool, session: ContainerOutput, msg: str) -> NoReturn:
             session.add_tag(fail_tag)
     raise GearExecutionError(msg)
 
+def get_record_id_suffix(
+        session: ContainerOutput,
+        proxy: FlywheelProxy) -> int:
+    """Determines the lowest suffix (>=1) that is greater than all others
 
-######### BEGIN SECTION FOR COMMON
+    Args:
+        session: target Flywheel session
+        proxy: FlywheelProxy to check for uniqueness of record_id on Flywheel
 
-# keys are Flywheel's two-character modality
-# values are NACC's three-character imagetype
-imagetype_from_modality = {
-    "PT": 1,  # 'PET'
-    "MR": 2,  # 'MRI',
-}
+    Returns:
+        The next unused record_id suffix
+    """
 
-pet_tag_for_variable = {
-    "tracer_dose_assay": "RadionuclideTotalDose",
-    "tracer_inj_time": "RadiopharmaceuticalStartDateTime",
-    "emission_start_time": "AcquisitionTime",
-}
-
-
-def find_flywheel_origin_user_id(
-    dry_run: bool,
-    session: ContainerOutput,
-    flywheel_obj: FileEntry,
-    proxy: FlywheelProxy,
-) -> Optional[str]:
-    match flywheel_obj.origin["type"]:
-        case "user":
-            return flywheel_obj.origin["id"]
-        case "job":
-            job = proxy.get_job_by_id(flywheel_obj.origin["id"])
-            if job is None:
-                tag_fail(
-                    dry_run,
-                    session,
-                    f"Unable to determine uploader for object {flywheel_obj.id}",
-                )
-                return None
-            try:
-                return job["config"]["inputs"]["input-file"]["object"]["origin"]["id"]
-            except (KeyError, TypeError, IndexError):
-                tag_fail(
-                    dry_run,
-                    session,
-                    f"Unable to determine uploader for object {flywheel_obj.id}",
-                )
-        case _:
-            tag_fail(
-                dry_run,
-                session,
-                f"Unable to determine uploader for object {flywheel_obj.id}",
-            )
-
-
-def inspect_acquisitions(  # noqa: C901
-    dry_run: bool,
-    session: ContainerOutput,
-    session_info_to_import: Dict[str, Any],
-    proxy: FlywheelProxy,
-) -> None:
-    def set_or_agree(key_to_set: str, val_to_set: Any, info_context: str):
-        if key_to_set not in session_info_to_import:
-            session_info_to_import[key_to_set] = val_to_set
-        elif session_info_to_import[key_to_set] != val_to_set:
-            tag_fail(
-                dry_run,
-                session,
-                f'Expected "{session_info_to_import[key_to_set]}" not "{val_to_set}" '
-                f"for {key_to_set} from {info_context}",
-            )
-
-    fw_mri_series = []
-    for acq in session.acquisitions():
-        log.info(f"  Found acquisition: {acq.label}")
-        for file in acq.files:
-            file = file.reload()  # needed to populate file.info
-            set_or_agree(
-                "uploader_email",
-                find_flywheel_origin_user_id(dry_run, session, file, proxy),
-                f'origin["id"] in {file.name}',
-            )
-            set_or_agree(
-                "imagetype",
-                imagetype_from_modality[file.modality],
-                f"file.modality in {file.name}",
-            )
-            studydt = file.info["header"]["dicom"]["StudyDate"]
-            studydt = studydt[:4] + "-" + studydt[4:6] + "-" + studydt[6:]
-            set_or_agree(
-                "scandt",
-                studydt,
-                f'file.info["header"]["dicom"]["StudyDate"] in {file.name}',
-            )
-            if file.modality == "PT":
-                for pet_var, pet_tag in pet_tag_for_variable.items():
-                    if pet_tag in file.info["header"]["dicom"]:
-                        set_or_agree(
-                            pet_var,
-                            file.info["header"]["dicom"][pet_tag],
-                            f'file.info["header"]["dicom"]["{pet_var}"] in {file.name}',
-                        )
-            elif file.modality == "MR":
-                # file.classification options: Features, Intent, and Measurement
-                fw_mri_series.append(
-                    ",".join(
-                        file.classification["Measurement"]
-                        + sorted(file.classification["Features"], reverse=True)
-                    )
-                    + ":"
-                    + file.info["header"]["dicom"]["SeriesDescription"]
-                )
-    if "scandt" not in session_info_to_import:
-        tag_fail(dry_run, session, "No scandt found from any acquisition")
-    if file.modality == "MR":
-        session_info_to_import["fw_mri_series"] = ";".join(fw_mri_series)
-
-
-# does not find or define record_id because record_id needs special treatment
-def construct_session_info_for_redcap(
-    dry_run: bool, session: ContainerOutput, proxy: FlywheelProxy
-) -> Dict[str, Any]:
-    fw_proj = proxy.get_container_by_id(session.project)
-
-    if "pipeline_adcid" in fw_proj.info:
-        adcid = fw_proj.info["pipeline_adcid"]
-    else:
-        tag_fail(
-            dry_run,
-            session,
-            "Expected pipeline_adcid key in custom information "
-            f"from project {fw_proj.label} for session {session.label}",
-        )
-    if not isinstance(adcid, int):
-        tag_fail(
-            dry_run, session, f"Expected adcid to be int, not {type(adcid)} for {adcid}"
-        )
-
-    subject = session.subject
-    if "naccid" not in subject.info:
-        tag_fail(dry_run, session, "Expected entry for naccid in subject.info")
-    session_info_to_import = {
-        "adcid": adcid,
-        "fw_session_label": session.label,
-        "fwid": session.id,
-        "ptid": session.subject.label,
-        "naccid": session.subject.info["naccid"],
-        "scanstart": session.timestamp.strftime("%H:%M:%S")
-        if session.timestamp
-        else "",
-    }
-
-    inspect_acquisitions(dry_run, session, session_info_to_import, proxy)
-    if "uploader_email" in session_info_to_import:
-        user = proxy.find_user(session_info_to_import["uploader_email"])
-        if user is None:
-            tag_fail(
-                dry_run,
-                session,
-                "Unable to determine uploader_fullname from "
-                f"email {session_info_to_import['uploader_email']}",
-            )
-        else:
-            session_info_to_import["uploader_fullname"] = (
-                (user.firstname or "") + " " + (user.lastname or "")
-            )
-    else:
-        tag_fail(
-            dry_run, session, "Missing uploader_email after inspecting acquisitions"
-        )
-
-    return session_info_to_import
-
-
-######### END SECTION FOR COMMON
-
-
-def get_record_id_suffix(session: ContainerOutput, proxy: FlywheelProxy) -> int:
     record_id_pattern = re.compile("^IMG[0-9]{2}_[0-9]{6}")
 
     fw_project = proxy.get_container_by_id(session.project)
@@ -216,9 +73,12 @@ def get_record_id_suffix(session: ContainerOutput, proxy: FlywheelProxy) -> int:
     for proj_ses in fw_project.sessions():
         if proj_ses.id == session.id:
             continue
+
+        # session's custom info not populating if
+        # grabbed directly from fw_project.sessions()
         ses = proxy.get_container_by_id(
             proj_ses.id
-        )  # custom info not populated from fw_project.sessions()
+        )
         ses.reload()
         if "record_id" in ses.info:
             existing_record_id = ses.info["record_id"]
@@ -233,8 +93,25 @@ def get_record_id_suffix(session: ContainerOutput, proxy: FlywheelProxy) -> int:
 
 
 def compose_record_id(
-    dry_run: bool, adcid: int, session: ContainerOutput, proxy: FlywheelProxy
+    dry_run: bool,
+    adcid: int,
+    session: ContainerOutput,
+    proxy: FlywheelProxy
 ) -> str:
+    """Composes the REDCap record_id in the format IMGSS_XXXXXX, where
+    SS is the site code (adcid) and the 6-digit suffix XXXXXX is different
+    than the others within the Flywheel project.
+
+    Args:
+        dry_run: flag for dry run (data collected but no modifications)
+        adcid: integer that identifies the ADC
+        session: target Flywheel session
+        proxy: FlywheelProxy to check for uniqueness of record_id on Flywheel
+
+    Returns:
+        The formatted string for record_id
+    """
+
     composed_id = (
         "IMG"
         + str(adcid).zfill(2)
@@ -246,31 +123,46 @@ def compose_record_id(
         session = proxy.get_container_by_id(session.id)
     return composed_id
 
-
 MAX_NEW_RECORD_ATTEMPTS = 4
 MAX_PAUSES_FOR_OTHER_SESSION = 3
 
+def ensure_record_id_is_unique(
+        dry_run: bool,
+        session: ContainerOutput,
+        proxy: FlywheelProxy,
+        adcid: int,
+        record_id: str
+) -> bool:
+    """Ensures record_id is unique if not a dry run; tries to resolve
+    possible collisions from simultaneous new sessions by waiting for
+    other sessions to yield the record_id or by changing its own record_id.
 
-def generate_unique_record_id(  # noqa: C901
-    dry_run: bool, session: ContainerOutput, proxy: FlywheelProxy, adcid: int
-) -> Optional[str]:
-    record_id = compose_record_id(dry_run, adcid, session, proxy)
-    if dry_run:
-        log.info(f"Dry run -- returning {record_id}")
-        return record_id
+    Args:
+        dry_run: flag for dry run (data collected but no modifications)
+        session: target Flywheel session
+        proxy: FlywheelProxy to check for uniqueness of record_id on Flywheel
+        adcid: integer that identifies the ADC
+        record_id: given initial record_id, but could be modified if not a dry \
+run
 
-    confirmed_record_id = False
+    Returns:
+        boolean indicating whether the record_id could be confirmed to be uniqu\
+e
+    """
+
+    able_to_confirm_record_id = False
     for i in range(MAX_NEW_RECORD_ATTEMPTS):
         fw_project = proxy.get_container_by_id(session.project)
-        confirmed_record_id = True
+        able_to_confirm_record_id = True
         for proj_ses in fw_project.sessions():
             if proj_ses.id == session.id:
                 continue
-            ses = proxy.get_container_by_id(
-                proj_ses.id
-            )  # custom info not populated from fw_project.sessions()
+
+            # session's custom info not populating if grabbed
+            # directly from fw_project.sessions()
+            ses = proxy.get_container_by_id(proj_ses.id)
             ses.reload()
-            if "record_id" in ses.info and ses.info["record_id"] == record_id:
+            if ses.info.get("record_id") == record_id:
                 log.info(
                     f"Conflict during pass {i + 1}/{MAX_NEW_RECORD_ATTEMPTS}"
                     f" in record_id {record_id}"
@@ -280,7 +172,7 @@ def generate_unique_record_id(  # noqa: C901
                 if int(ses.id, 16) < int(session.id, 16):
                     record_id = compose_record_id(dry_run, adcid, session, proxy)
                     log.info(f"Trying new record_id {record_id}")
-                    confirmed_record_id = False
+                    able_to_confirm_record_id = False
                     break
                 else:
                     for p in range(MAX_PAUSES_FOR_OTHER_SESSION):
@@ -289,9 +181,9 @@ def generate_unique_record_id(  # noqa: C901
                             f"{record_id} ({p + 1}/{MAX_PAUSES_FOR_OTHER_SESSION})"
                         )
                         sleep(1.0)
-                        ses = proxy.get_container_by_id(
-                            ses.id
-                        )  # reload() does not update ses.info['record_id']
+                        # reload() does not update ses.info["record_id"],
+                        # so get fresh container again
+                        ses = proxy.get_container_by_id(ses.id)
                         if ses.info["record_id"] != record_id:
                             log.info(f"  ...session {ses.id} yielded.")
                             break
@@ -299,10 +191,37 @@ def generate_unique_record_id(  # noqa: C901
                     if ses.info["record_id"] == record_id:
                         record_id = compose_record_id(dry_run, adcid, session, proxy)
                         log.info(f"  ...trying new record_id {record_id}")
-                        confirmed_record_id = False
+                        able_to_confirm_record_id = False
                         break
+    return able_to_confirm_record_id
 
-    if confirmed_record_id:
+def generate_unique_record_id(
+    dry_run: bool,
+    session: ContainerOutput,
+    proxy: FlywheelProxy,
+    adcid: int
+) -> Optional[str]:
+    """Generates a unique REDCap record_id for the session.
+
+    Args:
+        dry_run: flag for dry run (data collected but no modifications)
+        session: target Flywheel session
+        proxy: FlywheelProxy to check for uniqueness of record_id on Flywheel
+        adcid: integer that identifies the ADC
+
+    Returns:
+        The unique record_id (confirmed if not a dry run)
+
+    Raises:
+        GearExecutionError if a unique record_id cannot be secured
+    """
+
+    record_id = compose_record_id(dry_run, adcid, session, proxy)
+    if dry_run:
+        log.info(f"Dry run -- returning {record_id}")
+        return record_id
+
+    if ensure_record_id_is_unique(dry_run, session, proxy, adcid, record_id):
         return record_id
 
     tag_fail(
@@ -316,15 +235,27 @@ def generate_unique_record_id(  # noqa: C901
 def import_new_record_for_session(
     dry_run: bool,
     redcap_con: REDCapConnection,
-    session_info_to_import: Dict[str, Any],
+    session_info_to_import: FlywheelREDCapImageForm,
     session: ContainerOutput,
     proxy: FlywheelProxy,
 ) -> None:
+    """Imports the session's information into a new REDCap record.
+
+    Args:
+        dry_run: flag for dry run (data collected but no modifications)
+        redcap_con: connection for recap_project
+        session_info_to_import: dict-style information for REDCap form
+        session: target Flywheel session
+        proxy: FlywheelProxy to check for uniqueness of record_id on Flywheel
+
+    Raises:
+        GearExecutionError if a unique record_id cannot be secured
+    """
+
     record_id = generate_unique_record_id(
         dry_run, session, proxy, session_info_to_import["adcid"]
     )
-
-    final_session_info_to_import = dict()
+    final_session_info_to_import = {}
     final_session_info_to_import["record_id"] = record_id
     final_session_info_to_import.update(session_info_to_import)
     log.info("final session_info_to_import:")
@@ -348,7 +279,6 @@ def import_new_record_for_session(
     else:
         tag_fail(dry_run, session, "Unable to generate unique new record_id")
 
-
 def run(
     *,
     dry_run: bool,
@@ -357,12 +287,18 @@ def run(
     proxy: FlywheelProxy,
 ):
     """Runs the REDCap Image Form Creator process, collecting the available
-    information from the uploaded imaging for the REDCap form.
+    information from the uploaded image on Flywheel to be uploaded to the
+    REDCap image form.
 
     Args:
+        dry_run: flag for dry run (data collected but no modifications)
         session_id: Flywheel ID for the session
         redcap_con: API connection to REDCap project
         proxy: the proxy for the Flywheel instance
+
+    Raises:
+        GearExecutionError if critical information is not found
+        or the new record_id cannot be assigned
     """
 
     session = proxy.get_container_by_id(session_id)
@@ -381,8 +317,21 @@ def run(
     if session.info.get("record_id"):
         log.info(f"Note previous record_id for session is {session.info['record_id']}")
 
-    session_info_to_import = construct_session_info_for_redcap(dry_run, session, proxy)
+    session_info_to_import = FlywheelREDCapImageForm(session, proxy)
+    missed_some_key = False
+    for key_check in session_info_to_import.all_types_variables_to_check:
+        if session_info_to_import.get(key_check) is None:
+            log.warning(f"Missing {key_check}")
+            missed_some_key = True
+    if missed_some_key:
+        tag_fail(
+            dry_run,
+            session,
+            f"Missing information for {session.label} ({session.id})"
+        )
 
     import_new_record_for_session(
         dry_run, redcap_con, session_info_to_import, session, proxy
     )
+
+    log.info("Completed run from main")
